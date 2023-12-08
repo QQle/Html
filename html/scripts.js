995437b1ea678a6acc7facbdc6cf27f596c50bfd
Арслан Курбанов <arslik35@gmail.com>
2023-12-08 18:38:31 +0300

    Lab6

--- html/scripts.js
+++ html/scripts.js
@@ -1,17 +1,29 @@
-const fullPhoto = document.querySelector('.fullPhoto');
-const gallaryContainer= document.querySelector('.port-slider');
+var images = document.querySelectorAll('.smallimg')
+const BIGimage = document.querySelector('.bigimg');
+var sliderContainer = document.querySelectorAll('.smallimg');
+var sliderPrev = document.getElementById('prev');
+var sliderNext = document.getElementById('next');
+
+images.forEach(function (image) {
+	image.addEventListener('click', function (event) {
+		setBigImage(event);
+	});
+});
+
+function setBigImage(event) {
+	const clicked = event.target.src;
+	let bigImage = document.querySelector('.bigimg');
+	event.target.src = bigImage.src;
+	bigImage.src = clicked;
+
+}
 const inputName = document.getElementById('name')
 const inputPhone = document.getElementById('phone')
-const thumbnails = gallaryContainer.children;
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var btnCon = document.getElementById("conection");
 var span = document.getElementsByClassName("close")[0];
-for(let i = 0; i < thumbnails.length; i++){
-    thumbnails[i].addEventListener('click', function(){
-        fullPhoto.src = thumbnails[i].src;
-    });
-}
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -38,4 +50,23 @@ inputPhone.oninput = function() {
     this.value = this.value.replace(/[^a-zA-ZА-Яа-яЁё]/g,'');
 }
 
-  
+
+
+var sliderIndex = 0;
+const sliderLength = document.querySelectorAll('.smallimg').length;
+
+function position(index) {
+
+	BIGimage.src = sliderContainer[index].src;
+}
+
+sliderNext.addEventListener("click", () => {
+	sliderIndex = (sliderIndex + 1) % sliderLength;
+	position(sliderIndex);
+
+});
+sliderPrev.addEventListener("click", () => {
+	sliderIndex = (sliderIndex - 1 + sliderLength) % sliderLength;
+	position(sliderIndex);
+
+});
